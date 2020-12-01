56bb356e5f397a87336b52ce5d1e40cba884ebc1
Curtes Malteser <antonio_bastiao@hotmail.com>
2020-12-01 13:48:45 +0100

    added log press recognizer to map

--- Virtual Tourist/Controller/MapViewController.swift
+++ Virtual Tourist/Controller/MapViewController.swift
@@ -6,13 +6,40 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+
+    @IBOutlet weak var travelLocationsMap: MKMapView!
+
+    private lazy var longPressRecogniser: UILongPressGestureRecognizer = initLongPressGestureRecognizer()
+
+    func initLongPressGestureRecognizer() -> UILongPressGestureRecognizer {
+        let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: #selector(MapViewController.handleLongPress(_:)))
+        longPressRecogniser.minimumPressDuration = 1.0
+        return longPressRecogniser
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        travelLocationsMap.addGestureRecognizer(longPressRecogniser)
     }
 
+    deinit {
+        travelLocationsMap.removeGestureRecognizer(longPressRecogniser)
+    }
+
+    @objc private func handleLongPress(_ gestureRecognizer: UIGestureRecognizer) {
+        if gestureRecognizer.state != .began {
+            return
+        }
+
+        let touchPoint = gestureRecognizer.location(in: travelLocationsMap)
+        let touchMapCoordinate = travelLocationsMap.convert(touchPoint, toCoordinateFrom: travelLocationsMap)
+
+        print("touchMapCoordinate \(touchMapCoordinate)")
+    }
 }
 
