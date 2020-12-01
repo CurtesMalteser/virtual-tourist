552a47d4b4c9c73da27293ec70ca090411ecee3a
Curtes Malteser <antonio_bastiao@hotmail.com>
2020-12-01 10:13:11 +0100

    added map

--- Virtual Tourist/Controller/MapViewController.swift
+++ Virtual Tourist/Controller/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  Virtual Tourist
 //
 //  Created by António Bastião on 30.11.20.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
