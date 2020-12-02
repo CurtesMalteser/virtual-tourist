2795aea18b16fe51409d0395e8beaebec7cda7ca
Curtes Malteser <antonio_bastiao@hotmail.com>
2020-12-02 16:14:46 +0100

    added PhotoAlbumViewController

---
+++ Virtual Tourist/ViewController/PhotoAlbumViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoAlbumViewController.swift
+//  Virtual Tourist
+//
+//  Created by António Bastião on 02.12.20.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+}
