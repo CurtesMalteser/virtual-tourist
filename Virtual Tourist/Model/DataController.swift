8f3b038a0b45dcd227a5d6d42f2faa09a3e331b2
Curtes Malteser <antonio_bastiao@hotmail.com>
2020-12-06 22:31:03 +0100

    added background context

--- Virtual Tourist/Model/DataController.swift
+++ Virtual Tourist/Model/DataController.swift
@@ -14,6 +14,8 @@ class DataController {
 
     lazy var viewContext: NSManagedObjectContext = persistentContainer.viewContext
 
+    lazy var backgroundContext: NSManagedObjectContext = persistentContainer.newBackgroundContext()
+
     init(modelName: String) {
         persistentContainer = NSPersistentContainer(name: modelName)
     }
@@ -25,9 +27,19 @@ class DataController {
                 fatalError("Unresolved error \(String(describing: error?.localizedDescription))")
             }
 
+            self.configureContexts()
+
             completion?()
 
         }
+    }
+
+    func configureContexts() {
+        viewContext.automaticallyMergesChangesFromParent = true
+        backgroundContext.automaticallyMergesChangesFromParent = true
+
+        backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
     }
 
     // MARK: - Core Data Saving support
@@ -40,8 +52,8 @@ class DataController {
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                let error = error as NSError
+                fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         }
     }
